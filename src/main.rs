b287b5b91843ebe0bc6bb7144f82680c3cae57d4
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-09-15 12:42:41 -0600

    Creating connection to the postgres data base

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+use std::env;
+use dotenv::dotenv;
+use tokio_postgres::{NoTls, Error};
 
-fn main() {
-    println!("Hello postg");
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    dotenv().ok();
+    let connection_string: String = env::var("DATABASE_URL").unwrap();
+
+    // Connect to the database.
+    let (client, connection) = tokio_postgres::connect(&connection_string, NoTls).await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    Ok(())
 }
