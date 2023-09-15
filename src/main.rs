ed2cda6c4a9fc5cb4c5ce92a33108c07c7c43109
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-09-15 13:11:33 -0600

    Adding new changes

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
+mod utils;
+
 use std::env;
 use dotenv::dotenv;
 use tokio_postgres::{NoTls, Error};
+use crate::utils::crud;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    dotenv().ok();
-    let connection_string: String = env::var("DATABASE_URL").unwrap();
-
-    // Connect to the database.
-    let (client, connection) = tokio_postgres::connect(&connection_string, NoTls).await?;
-
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
+    let client = crud::establish_connection();
 
     Ok(())
 }
